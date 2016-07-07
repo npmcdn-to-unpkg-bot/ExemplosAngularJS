96f096de3a18562099c5a39b7ed7beacd558a22f
Marcos Lisboa <marcos.lisboa@hotmart.com.br>
2016-07-07 01:24:47 -0300

    Create Project - Book

--- book/src/main/java/br/com/antares/contact/ContactController.java
+++ book/src/main/java/br/com/antares/contact/ContactController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/drivers")
+@RequestMapping("/api/contacts")
 public class ContactController {
 
     @Autowired
